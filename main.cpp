efab1133caf24d68eeb3047032cae285b4e60d95
Michael Maguire <github@michaelmaguire.ca>
2025-03-21 23:40:23 +0100

    Better variable names

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-vector<int> bellmanFord(int number_of_nodes, vector<vector<int>>& edges, int src) {
-    vector<int> dist(number_of_nodes, numeric_limits<int>::max());
-    dist[src] = 0;
+vector<int> bellmanFord(int number_of_nodes, vector<vector<int>>& edges, int start_node) {
+    vector<int> distance_to(number_of_nodes, numeric_limits<int>::max());
+    distance_to[start_node] = 0;
 
     // Bellman-Ford should complete in (number_of_node - 1) iterations.
     // If we can still relax edges in the number_of_node-th iteration, then there is a negative weight cycle.
@@ -13,17 +13,17 @@ vector<int> bellmanFord(int number_of_nodes, vector<vector<int>>& edges, int src
         relaxation_took_place = false;
         for (vector<int>& edge : edges) {
             int u = edge[0], v = edge[1], wt = edge[2];
-            int new_dist_to_v = dist[u] + wt;
-            if (dist[u] != numeric_limits<int>::max() && new_dist_to_v < dist[v]) {
+            int new_dist_to_v = distance_to[u] + wt;
+            if (distance_to[u] != numeric_limits<int>::max() && new_dist_to_v < distance_to[v]) {
 
                 relaxation_took_place = true;
 
                 if(i == number_of_nodes - 1) {
                     cout << "Negative weight cycle found!";
-                    exit(-1);
+                    distance_to.clear();
                 }                
 
-                dist[v] = new_dist_to_v;
+                distance_to[v] = new_dist_to_v;
             }
         }
 
@@ -32,7 +32,7 @@ vector<int> bellmanFord(int number_of_nodes, vector<vector<int>>& edges, int src
         }
     }
 
-    return dist;
+    return distance_to;
 }
 
 
@@ -45,13 +45,18 @@ int main(int argc, char **argv)
     const int number_of_nodes = 6;
 
     // Each triplet { start node, end node, weight }
-    vector<vector<int>> edges = {{0, 1, 10}, {0, 5, 8}, {1, 3, 2}, 
+    vector<vector<int>> edges_with_weights = {{0, 1, 10}, {0, 5, 8}, {1, 3, 2}, 
                                  {2, 1, 1}, {3, 2, -2}, {4, 1, -4}, 
                                  {4, 3, -1}, {5, 4, 1} };
 
-    int src = 0;
-    vector<int> ans = bellmanFord(number_of_nodes, edges, src);
-    for (int dist : ans) 
-        cout << dist << " ";
+    int start_node = 0;
+    vector<int> answer = bellmanFord(number_of_nodes, edges_with_weights, start_node);
+    if( answer.empty()) {
+        cout << "Negative weight cycle found!";
+    } else {
+        for (int distance : answer) {
+            cout << distance << " ";
+        }
+    }
     return 0;    
 }
